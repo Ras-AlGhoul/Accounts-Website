bc9adb84170408afd87c6cc521cbf7b468dd4cbc
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-09-02 13:19:09 +0200

    intialize mongo database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,19 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
+
+const User = mongoose.model('User', {
+    name: String,
+    age: Number
+});
+const Task = mongoose.model('Task', {
+    task: String,
+    complete: Boolean
+});
+
+const task1 = new Task({
+    task: 'finish Project',
+    complete: true
+})
+
+task1.save().then(()=>console.log(task1)).catch(error=>console.log(error));
