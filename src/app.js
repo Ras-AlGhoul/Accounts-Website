0c5afb59959b078c71e62e2693b74293a7430dd1
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-09-04 18:16:45 +0200

    routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+// const router = require('./routes/router');
 const app = express();
+const router = express.Router();
 const port = process.env.Port || 3000;
+
+app.post('/users', (req,res)=>{
+    res.send('test!!')
+})
+
 app.listen(port,()=>{
-    console.log('app is up and runing');
+    console.log('app is up and runing' + port);
 })
